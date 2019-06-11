72184698c1f7cb246b11138e8ea2400933340dd3
flare <izflare@protonmail.com>
2019-06-12 02:01:14 +0900

    [add] encode by using POPPT

--- src/module/encode.rs
+++ src/module/encode.rs
@@ -3,127 +3,56 @@ extern crate strlib;
 
 use bit_vec::BitVec;
 use std::time::Instant;
-use std::cmp::{min, max};
-use strlib::delta;
 use super::{cfg::*};
 use super::{poppt::*};
+use strlib::ffenc;
 
 pub fn encode(g: &Grammar, bv: &mut BitVec) -> () {
 
     let start = Instant::now();
+    let mut p: POPPT = POPPT::new();
+    g.to_poppt(&mut p);
+    
+    let mut z: BitVec = BitVec::new();
+    let mut l: BitVec = BitVec::new();
 
-    fn u_to_bv(x: u32, logn: u32, bv: &mut BitVec) -> () {
-        let mut z = x;
-        z = z.rotate_right(logn);
-        for _ in 0..logn {
-            z = z.rotate_left(1);
-            bv.push(z % 2 == 1);
-        }
-    }
-    let mut v: Vec<u32> = Vec::new();
+    ffenc::encode(&p.terminal.iter().map(|x| *x as u32).collect::<Vec<u32>>(), &mut z);
+    ffenc::encode(&p.label, &mut l);
+    ffenc::to_bits(z.len() as u32, 32, bv);
+    for b in z {bv.push(b);}
+    ffenc::to_bits(p.bit.len() as u32, 32, bv);
+    for b in &p.bit {bv.push(b);}
+    for b in l {bv.push(b);}
 
-    let mut prev = 0;
-    let mut lr = BitVec::new();
-    let mut r: Vec<u32> = Vec::new();
-    let mut u = Vec::new();
-    let mut ren = 1;
-    for e in &g.rule {
-        let m = max(e[0], e[1]);
-        if prev <= m {
-            v.push(m - prev + 1);
-            ren += 1;
-        }
-        else {
-            v.push(m);
-            r.push(ren);
-            ren = 1;
-        }
-        lr.push(e[0] >= e[1]);
-        prev = m;
-        u.push(max(e[0], e[1]) - min(e[0], e[1]) + 1);
-    }
-    r.push(ren);
-    let sbitlen = std::usize::MAX.count_ones() - (g.terminal.len() + g.rule.len()).leading_zeros();
-
-    u_to_bv(g.terminal.len() as u32, 8, bv);
-    for e in &g.terminal {u_to_bv(*e as u32, 8, bv);}
-    u_to_bv(g.rule.len() as u32, 32, bv);
-    u_to_bv(sbitlen as u32, 32, bv);
-    for e in &u {u_to_bv(*e, sbitlen, bv);}
-    for bit in &lr {bv.push(bit);}
-    u_to_bv(g.sequence.len() as u32, 32, bv);
-    for e in &g.sequence {u_to_bv(*e, sbitlen, bv);}
-    delta::encode(&v, bv);
-    delta::encode(&r, bv);
     let end = start.elapsed();
-
     println!("[Result: bit encoding]");
-    println!("Increasing seq    : {:?}", r.len());
-    println!("Bit length        : {:?} [bits]", bv.len());
+    println!("POPPT bit vec len : {:?}", p.bit.len());
+    println!("Label length      : {:?}", p.label.len());
+    println!("Total bit length  : {:?} [bits]", bv.len());
     println!("{}.{:03} sec elapsed", end.as_secs(), end.subsec_nanos()/1_000_000);
 }
 
-pub fn decode(bv: &BitVec, w: &mut Vec<u8>) -> () {
-    let mut v: Vec<u32> = Vec::new();
-    let mut g: Grammar = Grammar::new();
-    let mut zlen = 0;
-    let mut c: u32 = 0;
-    let mut glen = 0;
-    let mut sbitlen = 0;
-    let mut u: Vec<u32> = Vec::new();
-    let mut lr: BitVec = BitVec::new();
-    let mut slen = 0;
-    let mut d: BitVec = BitVec::new();
 
+pub fn decode(bv: &BitVec, g: &mut Grammar) -> () {
+
+    let mut p: POPPT = POPPT::new();
+    let mut z: BitVec = BitVec::new();
+    let mut l: BitVec = BitVec::new();
+    let mut zlen: usize = 0;
+    let mut blen: usize = 0;
+    let mut v: Vec<u32> = Vec::new();
     for i in 0..bv.len() {
-        if i < 8 {
-            zlen <<= 1; if bv[i] {zlen += 1;}
-        }
-        else if i < 8 + zlen * 8 {
-            c <<= 1; if bv[i] {c += 1;}
-            if i % 8 == 7 {g.terminal.push(c as u8); c = 0;}
-        }
-        else if i < 40 + zlen * 8 {
-            glen <<= 1; if bv[i] {glen += 1;}
-        }
-        else if i < 72 + zlen * 8 {
-            sbitlen <<= 1; if bv[i] {sbitlen += 1;}
-        }
-        else if i < 72 + zlen * 8 + glen * sbitlen {
-            c <<= 1; if bv[i] {c += 1;}
-            if (i - (72 + zlen * 8)) % sbitlen == sbitlen - 1 {u.push(c as u32); c = 0;}
-        }
-        else if i < 72 + zlen * 8 + glen * sbitlen + glen {
-            lr.push(bv[i]);
-        }
-        else if i < 104 + zlen * 8 + glen * sbitlen + glen {
-            slen <<= 1; if bv[i] {slen += 1;}
-        }
-        else if i < 104 + zlen * 8 + glen * sbitlen + glen + slen * sbitlen {
-            c <<= 1; if bv[i] {c += 1;}
-            if (i - (104 + zlen * 8 + glen * sbitlen + glen)) % sbitlen == sbitlen - 1 {g.sequence.push(c as u32); c = 0;}
-        }
-        else {d.push(bv[i]);}
+        if i < 32 {zlen <<= 1; if bv[i] {zlen += 1;}}
+        else if i < 32 + zlen {z.push(bv[i]);}
+        else if i < 64 + zlen {blen <<= 1; if bv[i] {blen += 1;}}
+        else if i < 64 + zlen + blen {p.bit.push(bv[i]);}
+        else {l.push(bv[i]);}
     }
-    delta::decode(&d, &mut v);
-    let mut prev = 0;
-    let mut rpos = glen;
-    let mut ren = v[rpos];
-    for i in 0..glen {
-        if ren > 1 {
-            if lr[i] {g.rule.push(vec![prev + v[i] - 1, prev + v[i] - u[i]]);}
-            else {g.rule.push(vec![prev + v[i] - u[i], prev + v[i] - 1]);}
-            ren -= 1;
-            prev = prev + v[i] - 1;
-        }
-        else {
-            if lr[i] {g.rule.push(vec![v[i], v[i] - u[i] + 1]);}
-            else {g.rule.push(vec![v[i] - u[i] + 1, v[i]]);}
-            rpos += 1;
-            ren = v[rpos];
-            prev = v[i];
-        }
-    }
-    g.derive(w);
+
+    ffenc::decode(&z, &mut v);
+    p.terminal = v.iter().map(|x| *x as u8).collect::<Vec<u8>>();
+    ffenc::decode(&l, &mut p.label);
+
+    p.to_grammar(g);
 }
 
