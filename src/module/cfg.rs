b3d25ec734c8c02af4f76776c43b0001dec9adfa
flare <izflare@protonmail.com>
2019-06-12 00:53:48 +0900

    [add] cfg to poppt

--- src/module/cfg.rs
+++ src/module/cfg.rs
@@ -21,6 +21,38 @@ impl Grammar {
         }
         for c in &self.sequence {dfs(*c as usize, &self.terminal, &self.rule, w);}
     }
+
+    pub fn to_poppt(&self, p: &mut POPPT) -> () {
+        let mut m: Vec<Option<u32>> = vec![None; self.terminal.len() + self.rule.len()];
+        let mut var: u32 = self.terminal.len() as u32 + 1;
+        fn dfs(i: usize, z: &Vec<u8>, g: &Vec<Vec<u32>>, p: &mut POPPT, m: &mut Vec<Option<u32>>, var: &mut u32) -> () {
+            if let Some(x) = m[i - 1] {
+                p.label.push(x);
+            }
+            else {
+                if i < z.len() + 1 {
+                    m[i - 1] = Some(p.terminal.len() as u32 + 1);
+                    p.label.push(p.terminal.len() as u32 + 1);
+                    p.terminal.push(z[i - 1]);
+                }
+                else {
+                    let rs = &g[i - z.len() - 1];
+                    for e in rs {
+                        dfs(*e as usize, z, g, p, m, var);
+                    }
+                    for _ in rs {p.bit.push(false);}
+                    m[i - 1] = Some(*var);
+                    *var += 1;
+                }
+            }
+            p.bit.push(true);
+        }
+        for c in &self.sequence {
+            dfs(*c as usize, &self.terminal, &self.rule, p, &mut m, &mut var);
+        }
+        for _ in &self.sequence {p.bit.push(false);}
+        p.bit.push(true);
+    }
 }
 
 
--- src/module/poppt.rs
+++ src/module/poppt.rs
@@ -4,17 +4,18 @@ use bit_vec::BitVec;
 use super::{cfg::*};
 
 #[derive(Debug)]
-pub struct POPPT {pub bit: BitVec, pub label: Vec<u32>,}
+pub struct POPPT {pub bit: BitVec, pub label: Vec<u32>, pub terminal: Vec<u8>}
 
 impl POPPT {
     pub fn new() -> Self {
-        Self {bit: BitVec::new(), label: Vec::new(),}
+        Self {bit: BitVec::new(), label: Vec::new(), terminal: Vec::new(),}
     }
 
     pub fn to_grammar(&self, g: &mut Grammar) -> () {
         let mut right_side: Vec<u32> = Vec::new();
         let mut stack: Vec<u32> = Vec::new();
-        let mut var: u32 = g.terminal.len() as u32 + 1;
+        let mut var: u32 = self.terminal.len() as u32 + 1;
+        g.terminal = self.terminal.clone();
         let mut i = 0;
         for b in &self.bit {
             if b {
