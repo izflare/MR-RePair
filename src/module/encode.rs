8d8ea82fa1b22930f953f0559165ad0f7695e444
flare <izflare@protonmail.com>
2019-06-12 20:48:54 +0900

    [use] adaptive increasing fixed length encoding

--- src/module/encode.rs
+++ src/module/encode.rs
@@ -9,6 +9,17 @@ use strlib::ffenc;
 
 pub fn encode(g: &Grammar, bv: &mut BitVec) -> () {
 
+    fn adaptive_enc(v: &Vec<u32>, init: u32, bv: &mut BitVec) -> () {
+        let mut m: u32 = 0;
+        for e in v {if *e > m {m = *e;}}
+        ffenc::to_bits(32 - m.leading_zeros(), 32, bv);
+        let mut r = 32 - init.leading_zeros();
+        for i in 0..v.len() {
+            ffenc::to_bits(v[i], r, bv);
+            if (i as u32 + init) == 2_u32.pow(r) && r < 32 - m.leading_zeros() {r += 1;}
+        }
+    }
+
     let start = Instant::now();
     let mut p: POPPT = POPPT::new();
     g.to_poppt(&mut p);
@@ -17,12 +28,12 @@ pub fn encode(g: &Grammar, bv: &mut BitVec) -> () {
     let mut l: BitVec = BitVec::new();
 
     ffenc::encode(&p.terminal.iter().map(|x| *x as u32).collect::<Vec<u32>>(), &mut z);
-    ffenc::encode(&p.label, &mut l);
+    adaptive_enc(&p.label, p.terminal.len() as u32 + 1, &mut l);
     ffenc::to_bits(z.len() as u32, 32, bv);
     for b in z {bv.push(b);}
     ffenc::to_bits(p.bit.len() as u32, 32, bv);
     for b in &p.bit {bv.push(b);}
-    for b in l {bv.push(b);}
+    for b in &l {bv.push(b);}
 
     let end = start.elapsed();
     println!("[Result: bit encoding]");
@@ -30,10 +41,29 @@ pub fn encode(g: &Grammar, bv: &mut BitVec) -> () {
     println!("Label length      : {:?}", p.label.len());
     println!("Total bit length  : {:?} [bits]", bv.len());
     println!("{}.{:03} sec elapsed", end.as_secs(), end.subsec_nanos()/1_000_000);
+
 }
 
 
 pub fn decode(bv: &BitVec, g: &mut Grammar) -> () {
+
+    fn adaptive_dec(bv: &BitVec, init: u32, v: &mut Vec<u32>) -> () {
+        let mut r = 32 - init.leading_zeros();
+        let mut sum = 32;
+        let mut m: u32 = 0;
+        let mut u: u32 = 0;
+        for i in 0..bv.len() {
+            if i < 32 {m <<= 1; if bv[i] {m += 1;}}
+            else {
+                u <<= 1; if bv[i] {u += 1;}
+                if (i as u32 - sum) % r == (r - 1) {
+                    v.push(u); 
+                    u = 0;
+                    if (v.len() as u32 + init) == 2_u32.pow(r) && r < m {r += 1; sum = i as u32;}
+                }
+            }
+        }
+    }
 
     let mut p: POPPT = POPPT::new();
     let mut z: BitVec = BitVec::new();
@@ -51,8 +81,9 @@ pub fn decode(bv: &BitVec, g: &mut Grammar) -> () {
 
     ffenc::decode(&z, &mut v);
     p.terminal = v.iter().map(|x| *x as u8).collect::<Vec<u8>>();
-    ffenc::decode(&l, &mut p.label);
+    adaptive_dec(&l, p.terminal.len() as u32 + 1, &mut p.label);
 
     p.to_grammar(g);
+
 }
 
