33377bde63b8dd8cf05bd49a60501eeca35d9c41
flare <izflare@protonmail.com>
2019-07-20 19:26:10 +0900

    [add] POPPT+IBLE

---
+++ src/module/encode/text_32bit.rs
@@ -0,0 +1,64 @@
+extern crate bit_vec;
+extern crate strlib;
+
+use bit_vec::BitVec;
+use strlib::fble;
+use super::super::{cfg::*};
+
+pub fn encode(g: &Grammar, bv: &mut BitVec) -> () {
+
+    fble::to_bv(g.terminal.len() as u32, 32, bv);
+    for e in &g.terminal {fble::to_bv(*e as u32, 8, bv);}
+    fble::to_bv(g.rule.len() as u32 + g.rule.iter().fold(0, |sum, x| sum + x.len())  as u32, 32, bv);
+    for e in &g.rule {
+        fble::to_bv(e.len() as u32, 32, bv);
+        for f in e {
+            fble::to_bv(*f as u32, 32, bv);
+        }
+    }
+    for e in &g.sequence {fble::to_bv(*e, 32, bv);}
+
+}
+
+
+pub fn decode(bv: &BitVec, g: &mut Grammar) -> () {
+
+    let mut zlen = 0;
+    let mut z: u8 = 0;
+    let mut vlen = 0;
+    let mut a: u32 = 0;
+    let mut s: u32 = 0;
+    let mut sum = 40;
+    let mut block = 0;
+    let mut v: Vec<u32> = Vec::new();
+
+    for i in 8..bv.len() {
+        if i < 8 + 32 {zlen <<= 1; if bv[i] {zlen += 1;}}
+        else if i < 8 + 32 + zlen * 8 {
+            z <<= 1; if bv[i] {z += 1;}
+            if (i - sum) % 8 == 7 {g.terminal.push(z); z = 0; sum = i + 1;}
+        }
+        else if i < 8 + 32 * 2 + zlen * 8 {vlen <<= 1; if bv[i] {vlen += 1;}}
+        else if i < 8 + 32 * 2 + zlen * 8 + vlen * 32 {
+            if block == 0 {
+                a <<= 1; if bv[i] {a += 1;}
+                if (i - sum) % 32 == 31 {block = a; a = 0;}
+            }
+            else {
+                a <<= 1; if bv[i] {a += 1;}
+                if (i - sum) % 32 == 31 {
+                    v.push(a); a = 0; block -= 1; 
+                    if block == 0 {
+                        g.rule.push(v);
+                        v = Vec::new();
+                    }
+                }
+            }
+        }
+        else {
+            s <<= 1; if bv[i] {s += 1;}
+            if (i - sum) % 32 == 31 {g.sequence.push(s);}
+        }
+    }
+
+}
---
+++ src/module/encode/text_fble.rs
@@ -0,0 +1,61 @@
+extern crate bit_vec;
+extern crate strlib;
+
+use bit_vec::BitVec;
+use strlib::fble;
+use super::super::{cfg::*};
+
+pub fn encode(g: &Grammar, bv: &mut BitVec) -> () {
+
+    fble::to_bv(g.terminal.len() as u32, 32, bv);
+    fble::to_bv(g.rule.len() as u32 + g.rule.iter().fold(0, |sum, x| sum + x.len()) as u32, 32, bv);
+
+    let mut v: Vec<u32> = Vec::new();
+    for e in &g.terminal {v.push(*e as u32);}
+    for e in &g.rule {
+        v.push(e.len() as u32);
+        for f in e {v.push(*f);}
+    }
+    for e in &g.sequence {v.push(*e);}
+
+    let mut tbv = BitVec::new();
+    fble::encode(&v, &mut tbv);
+    fble::to_bv(tbv.len() as u32, 32, bv);
+    for b in &tbv {bv.push(b);}
+
+}
+
+
+pub fn decode(bv: &BitVec, g: &mut Grammar) -> () {
+
+    let mut zlen = 0;
+    let mut rlen = 0;
+    let mut dlen = 0;
+    let mut d = BitVec::new();
+
+    for i in 8..bv.len() {
+        if i < 8 + 32 {zlen <<= 1; if bv[i] {zlen += 1;}}
+        else if i < 8 + 32 * 2 {rlen <<= 1; if bv[i] {rlen += 1;}}
+        else if i < 8 + 32 * 3 {dlen <<= 1; if bv[i] {dlen += 1;}}
+        else if i < 8 + 32 * 3 + dlen {d.push(bv[i]);}
+    }
+
+    let mut v: Vec<u32> = Vec::new();
+    fble::decode(&d, &mut v);
+
+    let mut num = 0;
+    let mut block: Vec<u32> = Vec::new();
+    for i in 0..v.len() {
+        if i < zlen {g.terminal.push(v[i] as u8);}
+        else if i < zlen + rlen {
+            if num == 0 {num = v[i];}
+            else {
+                block.push(v[i]);
+                num -= 1;
+                if num == 0 {g.rule.push(block); block = Vec::new();}
+            }
+        }
+        else {g.sequence.push(v[i]);}
+    }
+
+}
